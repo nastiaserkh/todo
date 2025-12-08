f3533dfb2617b57d1a13157f97bd217d951756d6
nastiaserkh <nastiaserkhove@gmail.com>
2025-12-08 22:40:59 +0200

    update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-
+import './styles/index.scss';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './styles/index.scss';
